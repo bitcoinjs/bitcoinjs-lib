a4c2cc6dd4e5246ef348d49430ae3462f7c092f5
Daniel Cousens <daniel210x@gmail.com>
2014-04-08 21:53:33 +1000

    Removes pointless error utility function

--- src/util.js
+++ src/util.js
@@ -7,7 +7,3 @@ exports.sha256ripe160 = function (data) {
   var wordArray = RIPEMD160(SHA256(convert.bytesToWordArray(data)))
   return convert.wordArrayToBytes(wordArray)
 }
-
-exports.error = function (msg) {
-  throw new Error(msg)
-}
