bef0b9f7c6f5aa555d6018424cf65d1dd8ae8005
Julius Taylor <juliustayl@gmail.com>
2014-04-17 01:51:57 -0400

    multisiginput test

--- test/script.js
+++ test/script.js
@@ -4,11 +4,12 @@ var Address = require('../src/address.js')
 var Network = require('../src/network.js')
 var crypto = require('../').crypto
 var Convert = require('../src/convert.js')
+var Transaction = require('../src/transaction.js')
 var bytesToHex = Convert.bytesToHex
 var hexToBytes = Convert.hexToBytes
 
 describe('Script', function() {
-  var p2shScriptPubKey, pubkeyScriptPubkey, addressScriptSig
+  var p2shScriptPubKey, pubkeyScriptPubKey, addressScriptSig
 
   beforeEach(function(){
     p2shScriptPubKey = "a914e8c300c87986efa84c37c0519929019ef86eb5b487"
@@ -73,40 +74,63 @@ describe('Script', function() {
     var numSigs, script, multisig, network
 
     beforeEach(function() {
-      compressedPubKeys = ['02ea1297665dd733d444f31ec2581020004892cdaaf3dd6c0107c615afb839785f',
-        '02fab2dea1458990793f56f42e4a47dbf35a12a351f26fa5d7e0cc7447eaafa21f',
-        '036c6802ce7e8113723dd92cdb852e492ebb157a871ca532c3cb9ed08248ff0e19']
+      compressedPubKeys = ['02b685e778aff13e5bc660543a1d3948ece99c808446251a32f48bb6f82ca8fb46',
+          '03f0c62dc8f914090d46e735da87b7ec44a8022ee47137083cb26dbfdfb1526a3f',
+          '035d7fdff7601affab9adac3ad2e05d9ff12c0852c071874693e1d5608efa4a317'
+        ]
         numSigs = 2
         network = Network.mainnet.p2shVersion
+        script = Script.createMultiSigOutputScript(numSigs, compressedPubKeys.map(hexToBytes))
     })
 
     it('should create valid multi-sig address', function() {
-      script = Script.createMultiSigOutputScript(numSigs, compressedPubKeys.map(hexToBytes))
       multisig = crypto.hash160(script.buffer)
       var multiSigAddress = Address(multisig, network).toString()
 
       assert.ok(Address.validate(multiSigAddress))
       assert.equal(Address.getVersion(multiSigAddress), Network.mainnet.p2shVersion)
-      assert.equal(multiSigAddress,'32vYjxBb7pHJJyXgNk8UoK3BdRDxBzny2v')
+      assert.equal(multiSigAddress,'3FScAVa7ZPHrN7X911q3cnqKgMCidN5ykK')
     })
 
     it('should create valid redeemScript', function() {
       var redeemScript = script.buffer
       var deserialized = new Script(redeemScript)
-      var numOfSignatures = deserialized.chunks[deserialized.chunks.length - 2] - 80
+      var numOfPubKeys = deserialized.chunks[deserialized.chunks.length - 2] - 80
       var signaturesRequired = deserialized.chunks[0] - 80
-      var sigs = [
+      var pubKeys = [
         bytesToHex(deserialized.chunks[1]),
         bytesToHex(deserialized.chunks[2]),
         bytesToHex(deserialized.chunks[3])
       ]
 
-      assert.equal(numOfSignatures, 3)
+      assert.equal(numOfPubKeys, 3)
       assert.equal(signaturesRequired, 2)
-      assert.equal(sigs[0], '02ea1297665dd733d444f31ec2581020004892cdaaf3dd6c0107c615afb839785f')
-      assert.equal(sigs[1], '02fab2dea1458990793f56f42e4a47dbf35a12a351f26fa5d7e0cc7447eaafa21f')
-      assert.equal(sigs[2], '036c6802ce7e8113723dd92cdb852e492ebb157a871ca532c3cb9ed08248ff0e19')
-      assert.equal(Address(crypto.hash160(redeemScript), network).toString(), '32vYjxBb7pHJJyXgNk8UoK3BdRDxBzny2v')
+      assert.equal(pubKeys[0], compressedPubKeys[0])
+      assert.equal(pubKeys[1], compressedPubKeys[1])
+      assert.equal(pubKeys[2], compressedPubKeys[2])
+      assert.equal(Address(crypto.hash160(redeemScript), network).toString(), '3FScAVa7ZPHrN7X911q3cnqKgMCidN5ykK')
+    })
+
+    it('should create and sign multisig input', function(){
+      //TESTNET
+      var testnetPubKeys = ['023f7b3dc5b1e2fda2e1608e36e7e9e6c5e611ee18e522b942cd07210c787133aa',
+          '024f30f51041bcaab327ffa3ece49a37deb28dd97542f5a2ff12b250d1d896b371',
+          '03b9b9bdd7f057075c1a55653287b1a1ab9cc772c63b738b367893d59aaf4b1265'
+      ]
+
+      var script = Script.createMultiSigOutputScript(2, testnetPubKeys.map(hexToBytes))
+      var redeemScript = new Script(script.buffer)
+      var signaturesRequired = redeemScript.chunks[0] - 80
+      var tx = Transaction.Transaction()
+
+      tx.addInput('892a14296bed2be5e408f0fc39665d620815599b20f408f13fe7b2ada50fe432:0')
+      tx.addOutput('mvSTauyk761goSWMVkDDjE68zBP3CJNVmv',900000)
+      var signatures = [tx.p2shsign(0,redeemScriptcript.buffer,'e1fdbcc3a4fbfd432d45af7ae0bd9983e978d467c39ca9c14aef5789a7a683f7')]
+
+      tx.applyMultisigs(0,redeemScript.buffer,signatures[0])
+
+      assert.equal(signaturesRequired,2)
+      assert.equal(tx.ins[0].script,false)
     })
   })
 })
