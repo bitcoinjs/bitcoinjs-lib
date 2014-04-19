1bcc7cfd95133b0b49408cec3493241feacb44cc
Daniel Cousens <github@dcousens.com>
2014-04-20 05:41:20 +1000

    Adds shared base58 test vectors

---
+++ test/fixtures/base58.js
@@ -0,0 +1,198 @@
+module.exports = {
+  valid: [
+    {
+      encoded: {
+        hex: '800000000000000000000000000000000000000000000000000000000000000001014671fc3f',
+        string: 'KwDiBf89QgGbjEhKnhXJuH7LrciVrZi3qYjgd9M7rFU73sVHnoWn'
+      },
+      decoded: {
+        version: 128,
+        payload: '000000000000000000000000000000000000000000000000000000000000000101',
+        checksum: '4671fc3f'
+      }
+    },
+    {
+      encoded: {
+        hex: '00751e76e8199196d454941c45d1b3a323f1433bd6510d1634',
+        string: '1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH'
+      },
+      decoded: {
+        version: 0,
+        payload: '751e76e8199196d454941c45d1b3a323f1433bd6',
+        checksum: '510d1634'
+      }
+    },
+    {
+      encoded: {
+        hex: '80000000000000000000000000000000007777777777777777777777777777777701f0845454',
+        string: 'KwDiBf89QgGbjEhKnhXJuH7UChJaB4ZGvCvAAf7taDvRnbQnjxps'
+      },
+      decoded: {
+        version: 128,
+        payload: '000000000000000000000000000000007777777777777777777777777777777701',
+        checksum: 'f0845454'
+      }
+    },
+    {
+      encoded: {
+        hex: '006b420cc35afb4d96ea118dbf1fc8ef4b0990798cb4f216d9',
+        string: '1An8RkWzqVF9GvpQFVX5AM4jpFe43eoPfe'
+      },
+      decoded: {
+        version: 0,
+        payload: '6b420cc35afb4d96ea118dbf1fc8ef4b0990798c',
+        checksum: 'b4f216d9'
+      }
+    },
+    {
+      encoded: {
+        hex: '80fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd036414001608f2ef0',
+        string: 'L5oLkpV3aqBjhki6LmvChTCV6odsp4SXM6FfU2Gppt5kFLaHLuZ9'
+      },
+      decoded: {
+        version: 128,
+        payload: 'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd036414001',
+        checksum: '608f2ef0'
+      }
+    },
+    {
+      encoded: {
+        hex: '00adde4c73c7b9cee17da6c7b3e2b2eea1a0dcbe672c2b0787',
+        string: '1GrLCmVQXoyJXaPJQdqssNqwxvha1eUo2E'
+      },
+      decoded: {
+        version: 0,
+        payload: 'adde4c73c7b9cee17da6c7b3e2b2eea1a0dcbe67',
+        checksum: '2c2b0787'
+      }
+    },
+    {
+      encoded: {
+        hex: '806685bb38cf8d16d22741f00140622fccf3ab21fe6804d433140a4cf301e6b4dd01c5de2a2f',
+        string: 'Kzf11e3pDUh5ZRjcQKwvT3BeA25X5ErdccGatoTx8WJi6f49qzrr'
+      },
+      decoded: {
+        version: 128,
+        payload: '6685bb38cf8d16d22741f00140622fccf3ab21fe6804d433140a4cf301e6b4dd01',
+        checksum: 'c5de2a2f'
+      }
+    },
+    {
+      encoded: {
+        hex: '00cce94f5f00db418576b78c6a4dae2366fae89638d0b3efb8',
+        string: '1KgULoNDuBeHovVskY3MAfNFonzBMETt6B'
+      },
+      decoded: {
+        version: 0,
+        payload: 'cce94f5f00db418576b78c6a4dae2366fae89638',
+        checksum: 'd0b3efb8'
+      }
+    },
+    {
+      encoded: {
+        hex: 'ef00000000000000000000000000000000000000000000000000000000000000010184e38d1f',
+        string: 'cMahea7zqjxrtgAbB7LSGbcQUr1uX1ojuat9jZodMN87JcbXMTcA'
+      },
+      decoded: {
+        version: 239,
+        payload: '000000000000000000000000000000000000000000000000000000000000000101',
+        checksum: '84e38d1f'
+      }
+    },
+    {
+      encoded: {
+        hex: '6f751e76e8199196d454941c45d1b3a323f1433bd655c484e3',
+        string: 'mrCDrCybB6J1vRfbwM5hemdJz73FwDBC8r'
+      },
+      decoded: {
+        version: 111,
+        payload: '751e76e8199196d454941c45d1b3a323f1433bd6',
+        checksum: '55c484e3'
+      }
+    },
+    {
+      encoded: {
+        hex: 'ef0000000000000000000000000000000077777777777777777777777777777777014a7bd246',
+        string: 'cMahea7zqjxrtgAbB7LSGbcXpvbyqWexzF4dH5aQ5LaS3LRBaSUV'
+      },
+      decoded: {
+        version: 239,
+        payload: '000000000000000000000000000000007777777777777777777777777777777701',
+        checksum: '4a7bd246'
+      }
+    },
+    {
+      encoded: {
+        hex: '6f6b420cc35afb4d96ea118dbf1fc8ef4b0990798cc00f9280',
+        string: 'mqJ5iobyeWgQ43J1y4VSzGH4gFEkxw2dFV'
+      },
+      decoded: {
+        version: 111,
+        payload: '6b420cc35afb4d96ea118dbf1fc8ef4b0990798c',
+        checksum: 'c00f9280'
+      }
+    },
+    {
+      encoded: {
+        hex: 'effffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd03641400143ef4bf2',
+        string: 'cWALDjUu1tszsCBMjBjL4mhYj2wHUWYDR8Q8aSjLKzjkW5eBtpzu'
+      },
+      decoded: {
+        version: 239,
+        payload: 'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd036414001',
+        checksum: '43ef4bf2'
+      }
+    },
+    {
+      encoded: {
+        hex: '6fadde4c73c7b9cee17da6c7b3e2b2eea1a0dcbe670a342a51',
+        string: 'mwNHVpaPLqQZJgrv8CpFhJ4GpvJGumskXi'
+      },
+      decoded: {
+        version: 111,
+        payload: 'adde4c73c7b9cee17da6c7b3e2b2eea1a0dcbe67',
+        checksum: '0a342a51'
+      }
+    },
+    {
+      encoded: {
+        hex: 'ef6685bb38cf8d16d22741f00140622fccf3ab21fe6804d433140a4cf301e6b4dd0124df07e1',
+        string: 'cR1zUZ3feYPLisCsnjm3pMghnFNvjgxKgeR41DvTdcxiMQ4g9ZYC'
+      },
+      decoded: {
+        version: 239,
+        payload: '6685bb38cf8d16d22741f00140622fccf3ab21fe6804d433140a4cf301e6b4dd01',
+        checksum: '24df07e1'
+      }
+    },
+    {
+      encoded: {
+        hex: '6fcce94f5f00db418576b78c6a4dae2366fae8963831693070',
+        string: 'mzCRdrTCiD5Yb2yVU71izaaafnatCA4DCT'
+      },
+      decoded: {
+        version: 111,
+        payload: 'cce94f5f00db418576b78c6a4dae2366fae89638',
+        checksum: '31693070'
+      }
+    }
+  ],
+  invalid: [
+    'fN2C5x86J',
+    'BiLLNZvcHTJWoL',
+    'MP3zZBT27cEeP4asJD',
+    'kfjbP4kdqZcZ5tThHpxmgWQ8vk',
+    'oM6DB1STJKkv9NtPs2vHer6yUBh',
+    'wkC5tvzZHuKmnrWwVMioYueXCSx',
+    'BoW61uTv9aHNZtfZzcGXMSJvuc3',
+    'HhRv5VFtWmf6y28fWUC4WijZ6UV',
+    '2QPqahfnSzPRScGyaCSFL8JUZkar',
+    '2RpPJRJyxYz1MTShU2gV8Gy1oDdM',
+    '2p1tshmtxKWeovXFe68Qu6wNFNqg',
+    '2bYahuZyEDrw12USasbLdxGfkDH9',
+    '2Pfp453t1TEkj116QEDaW5hkmf3y',
+    '47aZqGXrM3XzEy5mWs3wC4guA7hJ',
+    'BUaVB8rxkTyTPntSRV4sw32vLKUZS',
+    '2S7rqjHw5yypNZ7WrCCAr9HCQ9Va46WJnNb'
+  ]
+}
