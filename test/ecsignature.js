626f8fb22022fc70a42f74c75779af684e50a994
Daniel Cousens <github@dcousens.com>
2014-06-15 17:15:17 +1000

    ECSignature: add tests for scriptSignature

--- test/ecsignature.js
+++ test/ecsignature.js
@@ -79,4 +79,41 @@ describe('ECSignature', function() {
       })
     })
   })
+
+  describe('toScriptSignature', function() {
+    it('encodes a script signature', function() {
+      fixtures.valid.forEach(function(f) {
+        var signature = new ECSignature(
+          new BigInteger(f.signature.r),
+          new BigInteger(f.signature.s)
+        )
+
+        var scriptSignature = signature.toScriptSignature(f.scriptSignature.hashType)
+        assert.equal(scriptSignature.toString('hex'), f.scriptSignature.hex)
+      })
+    })
+  })
+
+  describe('fromScriptSignature', function() {
+    it('decodes the correct signature', function() {
+      fixtures.valid.forEach(function(f) {
+        var buffer = new Buffer(f.scriptSignature.hex, 'hex')
+        var parsed = ECSignature.fromScriptSignature(buffer)
+
+        assert.equal(parsed.signature.r.toString(), f.signature.r)
+        assert.equal(parsed.signature.s.toString(), f.signature.s)
+        assert.equal(parsed.hashType, f.scriptSignature.hashType)
+      })
+    })
+
+    fixtures.invalid.DER.forEach(function(f) {
+      it('throws on ' + f.hex, function() {
+        var buffer = new Buffer(f.hex + '01', 'hex')
+
+        assert.throws(function() {
+          ECSignature.fromScriptSignature(buffer)
+        }, new RegExp(f.exception))
+      })
+    })
+  })
 })
