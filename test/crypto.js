ac7b338cbcbe9c620b0f980d088739c4ac27c059
Daniel Cousens <github@dcousens.com>
2014-10-03 21:41:18 +1000

    crypto: re-work fixtures for simplicity

--- test/crypto.js
+++ test/crypto.js
@@ -1,60 +1,21 @@
 var assert = require('assert')
 var crypto = require('../src/crypto')
 
-var fixtures = require('./fixtures/crypto.json')
+var fixtures = require('./fixtures/crypto')
 
 describe('Crypto', function() {
-  describe('HASH160', function() {
-    it('matches the test vectors', function() {
-      fixtures.before.hex.forEach(function(hex, i) {
-        var data = new Buffer(hex, 'hex')
-        var actual = crypto.hash160(data).toString('hex')
+  ['hash160', 'hash256', 'ripemd160', 'sha1', 'sha256'].forEach(function(algorithm) {
+    describe(algorithm, function() {
+      fixtures.valid.forEach(function(f) {
+        var fn = crypto[algorithm]
+        var expected = f[algorithm]
 
-        assert.equal(actual, fixtures.after.hash160[i])
-      })
-    })
-  })
+        it('returns ' + expected + ' for ' + f.hex, function() {
+          var data = new Buffer(f.hex, 'hex')
+          var actual = fn(data).toString('hex')
 
-  describe('HASH256', function() {
-    it('matches the test vectors', function() {
-      fixtures.before.hex.forEach(function(hex, i) {
-        var data = new Buffer(hex, 'hex')
-        var actual = crypto.hash256(data).toString('hex')
-
-        assert.equal(actual, fixtures.after.hash256[i])
-      })
-    })
-  })
-
-  describe('RIPEMD160', function() {
-    it('matches the test vectors', function() {
-      fixtures.before.hex.forEach(function(hex, i) {
-        var data = new Buffer(hex, 'hex')
-        var actual = crypto.ripemd160(data).toString('hex')
-
-        assert.equal(actual, fixtures.after.ripemd160[i])
-      })
-    })
-  })
-
-  describe('SHA1', function() {
-    it('matches the test vectors', function() {
-      fixtures.before.hex.forEach(function(hex, i) {
-        var data = new Buffer(hex, 'hex')
-        var actual = crypto.sha1(data).toString('hex')
-
-        assert.equal(actual, fixtures.after.sha1[i])
-      })
-    })
-  })
-
-  describe('SHA256', function() {
-    it('matches the test vectors', function() {
-      fixtures.before.hex.forEach(function(hex, i) {
-        var data = new Buffer(hex, 'hex')
-        var actual = crypto.sha256(data).toString('hex')
-
-        assert.equal(actual, fixtures.after.sha256[i])
+          assert.equal(actual, expected)
+        })
       })
     })
   })
