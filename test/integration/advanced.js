837e0a3564716b0147e00e93a2128d4b353fbe49
Daniel Cousens <github@dcousens.com>
2014-12-09 11:54:36 +1100

    package: use cb-helloblock for integration tests

--- test/integration/advanced.js
+++ test/integration/advanced.js
@@ -1,8 +1,6 @@
 var assert = require('assert')
 var bitcoin = require('../../')
-var helloblock = require('helloblock-js')({
-  network: 'testnet'
-})
+var blockchain = new (require('cb-helloblock'))('testnet')
 
 describe('bitcoinjs-lib (advanced)', function() {
   it('can sign a Bitcoin message', function() {
@@ -27,10 +25,10 @@ describe('bitcoinjs-lib (advanced)', function() {
     var key = bitcoin.ECKey.fromWIF("L1uyy5qTuGrVXrmrsvHWHgVzW9kKdrp27wBC7Vs6nZDTF2BRUVwy")
     var address = key.pub.getAddress(bitcoin.networks.testnet).toString()
 
-    helloblock.faucet.withdraw(address, 2e4, function(err) {
+    blockchain.addresses.__faucetWithdraw(address, 2e4, function(err) {
       if (err) return done(err)
 
-      helloblock.addresses.getUnspents(address, function(err, _, unspents) {
+      blockchain.addresses.unspents(address, function(err, unspents) {
         if (err) return done(err)
 
         // filter small unspents
@@ -44,20 +42,19 @@ describe('bitcoinjs-lib (advanced)', function() {
         var data = new Buffer('cafedeadbeef', 'hex')
         var dataScript = bitcoin.scripts.nullDataOutput(data)
 
-        tx.addInput(unspent.txHash, unspent.index)
+        tx.addInput(unspent.txId, unspent.vout)
         tx.addOutput(dataScript, 1000)
         tx.sign(0, key)
 
-        helloblock.transactions.propagate(tx.build().toHex(), function(err) {
+        blockchain.transactions.propagate(tx.build().toHex(), function(err) {
           if (err) return done(err)
 
           // check that the message was propagated
-          helloblock.addresses.getTransactions(address, function(err, res, transactions) {
+          blockchain.addresses.transactions(address, function(err, transactions) {
             if (err) return done(err)
 
-            var transaction = transactions[0]
-            var output = transaction.outputs[0]
-            var dataScript2 = bitcoin.Script.fromHex(output.scriptPubKey)
+            var transaction = bitcoin.Transaction.fromHex(transactions[0].txHex)
+            var dataScript2 = transaction.outs[0].script
             var data2 = dataScript2.chunks[1]
 
             assert.deepEqual(dataScript, dataScript2)
