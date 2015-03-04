87bd9f5a8052c41a5f3ea23abb77dc073cf14931
Jason Serafin <jtserafin@gmail.com>
2015-03-04 16:30:13 -0500

    Get address from public key output script.

--- src/address.js
+++ src/address.js
@@ -3,6 +3,7 @@ var base58check = require('bs58check')
 var typeForce = require('typeforce')
 var networks = require('./networks')
 var scripts = require('./scripts')
+var crypto = require('./crypto')
 
 function findScriptTypeByVersion (version) {
   for (var networkName in networks) {
@@ -36,6 +37,7 @@ Address.fromOutputScript = function (script, network) {
 
   if (scripts.isPubKeyHashOutput(script)) return new Address(script.chunks[2], network.pubKeyHash)
   if (scripts.isScriptHashOutput(script)) return new Address(script.chunks[1], network.scriptHash)
+  if (scripts.isPubKeyOutput(script)) return new Address(crypto.hash160(script.chunks[0]), network.pubKeyHash)
 
   assert(false, script.toASM() + ' has no matching Address')
 }
