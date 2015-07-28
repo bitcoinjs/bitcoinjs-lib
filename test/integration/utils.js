430cdde7668ce2d65572dd7ca2fec5532bbbb845
Daniel Cousens <dcousens@users.noreply.github.com>
2015-07-28 10:49:23 +1000

    utils: extract BLOCKTRAIL_API_KEY to be configurable

--- test/integration/utils.js
+++ test/integration/utils.js
@@ -1,9 +1,11 @@
 var httpify = require('httpify')
 
+var BLOCKTRAIL_API_KEY = process.env.BLOCKTRAIL_API_KEY || 'c0bd8155c66e3fb148bb1664adc1e4dacd872548'
+
 function faucetWithdraw (address, amount, callback) {
   httpify({
     method: 'POST',
-    url: 'https://api.blocktrail.com/v1/tBTC/faucet/withdrawl?api_key=c0bd8155c66e3fb148bb1664adc1e4dacd872548',
+    url: 'https://api.blocktrail.com/v1/tBTC/faucet/withdrawl?api_key=' + BLOCKTRAIL_API_KEY,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       address: address,
