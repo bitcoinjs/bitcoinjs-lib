74fd5ae71d0eaaa1115b45a4af99103e9fcf51df
Daniel Cousens <github@dcousens.com>
2015-11-06 10:49:06 +1100

    tests: loop faucet until an unspent is given

--- test/integration/_blockchain.js
+++ test/integration/_blockchain.js
@@ -1,3 +1,4 @@
+var async = require('async')
 var Blockchain = require('cb-http-client')
 var httpify = require('httpify')
 
@@ -5,16 +6,42 @@ var BLOCKTRAIL_API_KEY = process.env.BLOCKTRAIL_API_KEY || 'c0bd8155c66e3fb148bb
 
 var mainnet = new Blockchain('https://api.blocktrail.com/cb/v0.2.1/BTC', { api_key: BLOCKTRAIL_API_KEY })
 var testnet = new Blockchain('https://api.blocktrail.com/cb/v0.2.1/tBTC', { api_key: BLOCKTRAIL_API_KEY })
-testnet.faucet = function faucet (address, amount, callback) {
-  httpify({
-    method: 'POST',
-    url: 'https://api.blocktrail.com/v1/tBTC/faucet/withdrawl?api_key=' + BLOCKTRAIL_API_KEY,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      address: address,
-      amount: amount
-    })
-  }, callback)
+
+testnet.faucet = function faucet (address, amount, done) {
+  var unspents = []
+
+  async.whilst(
+    function condition () { return unspents.length === 0 },
+    function f (callback) {
+      httpify({
+        method: 'POST',
+        url: 'https://api.blocktrail.com/v1/tBTC/faucet/withdrawl?api_key=' + BLOCKTRAIL_API_KEY,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          address: address,
+          amount: amount
+        })
+      }, function (err) {
+        if (err) return callback(err)
+
+        testnet.addresses.unspents(address, function (err, result) {
+          if (err) return callback(err)
+
+          // filter small unspents
+          unspents = result.filter(function (unspent) {
+            return unspent.value > 1e3
+          })
+
+          callback()
+        })
+      })
+    },
+    function (err) {
+      if (err) return done(err)
+
+      done(null, unspents)
+    }
+  )
 }
 
 module.exports = {
--- test/integration/advanced.js
+++ test/integration/advanced.js
@@ -22,53 +22,42 @@ describe('bitcoinjs-lib (advanced)', function () {
   })
 
   it('can create an OP_RETURN transaction', function (done) {
-    this.timeout(20000)
+    this.timeout(30000)
 
     var network = bitcoin.networks.testnet
     var keyPair = bitcoin.ECPair.makeRandom({ network: network })
     var address = keyPair.getAddress()
 
-    blockchain.t.faucet(address, 2e4, function (err) {
+    blockchain.t.faucet(address, 2e4, function (err, unspents) {
       if (err) return done(err)
 
-      blockchain.t.addresses.unspents(address, function (err, unspents) {
+      // use the oldest unspent
+      var unspent = unspents.pop()
+      var tx = new bitcoin.TransactionBuilder(network)
+      var data = new Buffer('bitcoinjs-lib')
+      var dataScript = bitcoin.script.nullDataOutput(data)
+
+      tx.addInput(unspent.txId, unspent.vout)
+      tx.addOutput(dataScript, 1000)
+      tx.sign(0, keyPair)
+
+      var txBuilt = tx.build()
+
+      blockchain.t.transactions.propagate(txBuilt.toHex(), function (err) {
         if (err) return done(err)
 
-        // filter small unspents
-        unspents = unspents.filter(function (unspent) {
-          return unspent.value > 1e4
-        })
-
-        // use the oldest unspent
-        var unspent = unspents.pop()
-        if (!unspent) throw new Error('Faucet didn\'t provide an unspent')
-
-        var tx = new bitcoin.TransactionBuilder(network)
-        var data = new Buffer('bitcoinjs-lib')
-        var dataScript = bitcoin.script.nullDataOutput(data)
-
-        tx.addInput(unspent.txId, unspent.vout)
-        tx.addOutput(dataScript, 1000)
-        tx.sign(0, keyPair)
-
-        var txBuilt = tx.build()
-
-        blockchain.t.transactions.propagate(txBuilt.toHex(), function (err) {
+        // check that the message was propagated
+        blockchain.t.transactions.get(txBuilt.getId(), function (err, transaction) {
           if (err) return done(err)
 
-          // check that the message was propagated
-          blockchain.t.transactions.get(txBuilt.getId(), function (err, transaction) {
-            if (err) return done(err)
+          var actual = bitcoin.Transaction.fromHex(transaction.txHex)
+          var dataScript2 = actual.outs[0].script
+          var data2 = bitcoin.script.decompile(dataScript2)[1]
 
-            var actual = bitcoin.Transaction.fromHex(transaction.txHex)
-            var dataScript2 = actual.outs[0].script
-            var data2 = bitcoin.script.decompile(dataScript2)[1]
+          assert.deepEqual(dataScript, dataScript2)
+          assert.deepEqual(data, data2)
 
-            assert.deepEqual(dataScript, dataScript2)
-            assert.deepEqual(data, data2)
-
-            done()
-          })
+          done()
         })
       })
     })
