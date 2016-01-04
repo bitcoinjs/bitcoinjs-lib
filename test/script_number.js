059f48b0c426f9a0c901bc0b9c1fef09b29c2632
Daniel Cousens <github@dcousens.com>
2016-01-05 01:59:14 +1100

    script_number: add impl/tests

---
+++ test/script_number.js
@@ -0,0 +1,27 @@
+/* global describe, it */
+
+var assert = require('assert')
+var scriptNumber = require('../src/script_number')
+var fixtures = require('./fixtures/script_number.json')
+
+describe('script', function () {
+  describe('decode', function () {
+    fixtures.forEach(function (f) {
+      it(f.hex + ' returns ' + f.number, function () {
+        var actual = scriptNumber.decode(new Buffer(f.hex, 'hex'), f.bytes)
+
+        assert.strictEqual(actual, f.number)
+      })
+    })
+  })
+
+  describe('encode', function () {
+    fixtures.forEach(function (f) {
+      it(f.number + ' returns ' + f.hex, function () {
+        var actual = scriptNumber.encode(f.number)
+
+        assert.strictEqual(actual.toString('hex'), f.hex)
+      })
+    })
+  })
+})
