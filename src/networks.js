49b539cae7214d3309aad80c3b5b2ab0b0b51976
3s3s <ivanivanovkzv@gmail.com>
2016-03-17 23:22:37 +0500

    add dash and sib

--- src/networks.js
+++ src/networks.js
@@ -46,4 +46,38 @@ module.exports = {
     wif: 0x9e,
     dustThreshold: 0 // https://github.com/dogecoin/dogecoin/blob/v1.7.1/src/core.h#L155-L160
   }
+  dogecoin: {
+    messagePrefix: '\x19Dogecoin Signed Message:\n',
+    bip32: {
+      public: 0x02facafd,
+      private: 0x02fac398
+    },
+    pubKeyHash: 0x1e,
+    scriptHash: 0x16,
+    wif: 0x9e,
+    dustThreshold: 0 // https://github.com/dogecoin/dogecoin/blob/v1.7.1/src/core.h#L155-L160
+  },
+  dash: {
+    messagePrefix: '\x19DarkCoin Signed Message:\n', // https://github.com/dashpay/dash/blob/master/src/main.cpp#L90
+    bip32: {
+      public: 0x02fe52f8, // https://github.com/dashpay/dash/blob/master/src/chainparams.cpp#L172
+      private: 0x02fe52cc // https://github.com/dashpay/dash/blob/master/src/chainparams.cpp#L173
+    },
+    pubKeyHash: 0x4c, // https://github.com/dashpay/dash/blob/master/src/chainparams.cpp#L169
+    scriptHash: 0x10, // https://github.com/dashpay/dash/blob/master/src/chainparams.cpp#L170
+    wif: 0xcc, // https://github.com/dashpay/dash/blob/master/src/chainparams.cpp#L171
+    dustThreshold: 5460 // https://github.com/dashpay/dash/blob/v0.12.0.x/src/primitives/transaction.h#L144-L155
+  },
+  sib: {
+    messagePrefix: '\x18SibCoin Signed Message:\n', // https://github.com/ivansib/sibcoin/blob/master/src/main.cpp#L91
+    bip32: {
+      public: 0x0488b21e, // https://github.com/ivansib/sibcoin/blob/master/src/chainparams.cpp#L175
+      private: 0x0488ade4 // https://github.com/ivansib/sibcoin/blob/master/src/chainparams.cpp#L176
+    },
+    pubKeyHash: 0x3f, // https://github.com/ivansib/sibcoin/blob/master/src/chainparams.cpp#L172
+    scriptHash: 0x28, // https://github.com/ivansib/sibcoin/blob/master/src/chainparams.cpp#L173
+    wif: 0x80, // https://github.com/ivansib/sibcoin/blob/master/src/chainparams.cpp#L174
+    dustThreshold: 5460 // https://github.com/ivansib/sibcoin/blob/master/src/primitives/transaction.h#L144-L155
+  }
+  
 }
