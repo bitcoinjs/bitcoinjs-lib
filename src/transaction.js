736f92d67e8381c05a24b4ada5441a8ea14c2310
Daniel Cousens <github@dcousens.com>
2016-10-10 11:08:30 +1100

    Transaction: multiline if formatting

--- src/transaction.js
+++ src/transaction.js
@@ -315,7 +315,9 @@ Transaction.prototype.hashForWitnessV0 = function (inIndex, prevOutScript, amoun
     hashPrevouts = bcrypto.hash256(tbuffer)
   }
 
-  if (!(hashType & Transaction.SIGHASH_ANYONECANPAY) && (hashType & 0x1f) !== Transaction.SIGHASH_SINGLE && (hashType & 0x1f) !== Transaction.SIGHASH_NONE) {
+  if (!(hashType & Transaction.SIGHASH_ANYONECANPAY) &&
+       (hashType & 0x1f) !== Transaction.SIGHASH_SINGLE &&
+       (hashType & 0x1f) !== Transaction.SIGHASH_NONE) {
     tbuffer = new Buffer(4 * this.ins.length)
     toffset = 0
 
@@ -326,7 +328,8 @@ Transaction.prototype.hashForWitnessV0 = function (inIndex, prevOutScript, amoun
     hashSequence = bcrypto.hash256(tbuffer)
   }
 
-  if ((hashType & 0x1f) !== Transaction.SIGHASH_SINGLE && (hashType & 0x1f) !== Transaction.SIGHASH_NONE) {
+  if ((hashType & 0x1f) !== Transaction.SIGHASH_SINGLE &&
+      (hashType & 0x1f) !== Transaction.SIGHASH_NONE) {
     var txOutsSize = this.outs.reduce(function (sum, output) {
       return sum + 8 + scriptSize(output.script)
     }, 0)
