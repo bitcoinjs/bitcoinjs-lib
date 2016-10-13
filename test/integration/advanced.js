9d67af61d8f6e5bf86f16c7ee5f4cb288a2aece8
Daniel Cousens <github@dcousens.com>
2016-10-13 17:33:55 +1100

    integration: break up into more re-usable unspents

--- test/integration/advanced.js
+++ test/integration/advanced.js
@@ -11,7 +11,7 @@ describe('bitcoinjs-lib (advanced)', function () {
     var keyPair = bitcoin.ECPair.makeRandom({ network: network })
     var address = keyPair.getAddress()
 
-    blockchain.t.faucet(address, 2e4, function (err, unspent) {
+    blockchain.t.faucet(address, 5e4, function (err, unspent) {
       if (err) return done(err)
 
       var tx = new bitcoin.TransactionBuilder(network)
@@ -20,7 +20,7 @@ describe('bitcoinjs-lib (advanced)', function () {
 
       tx.addInput(unspent.txId, unspent.vout)
       tx.addOutput(dataScript, 1000)
-      tx.addOutput(blockchain.t.RETURN, 1e4)
+      tx.addOutput(blockchain.t.RETURN, 4e4)
       tx.sign(0, keyPair)
       var txRaw = tx.build()
 
