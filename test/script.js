a56de14cc53e114b306bf0b2d34881acc4c68c74
Thomas Kerin <me@thomaskerin.io>
2016-11-09 17:13:14 +0100

    Solve Output: take an output script, returns {type, script, solvedBy, requiredSigs, canSign}

--- test/script.js
+++ test/script.js
@@ -118,6 +118,30 @@ describe('script', function () {
     })
   })
 
+  describe('solveOutput', function () {
+    fixtures.valid.forEach(function (f) {
+      var script
+      if (f.scriptPubKeyHex) {
+        script = new Buffer(f.scriptPubKeyHex, 'hex')
+      } else if (f.scriptPubKey) {
+        script = bscript.fromASM(f.scriptPubKey)
+      } else {
+        return
+      }
+
+      it('solves ' + bscript.toASM(script) + ' as ' + f.type, function () {
+        var solution = bscript.solveOutput(script)
+        assert.equal(solution.type, f.type)
+        if ([bscript.types.P2SH].concat(bscript.P2SH_SCRIPTS).indexOf(f.type) === -1) {
+          assert.equal(solution.solvedBy, null)
+        }
+        if (solution.canSign) {
+          assert.notEqual(solution.requiredSigs, null)
+        }
+      })
+    })
+  })
+
   describe('SCRIPT_VERIFY_MINIMALDATA policy', function () {
     fixtures.valid.forEach(function (f) {
       if (f.scriptSigHex) {
