1f21e9b610980977f0561940bde268750c667855
Daniel Cousens <github@dcousens.com>
2017-08-14 17:15:34 +1000

    add from/toBech32

--- src/address.js
+++ src/address.js
@@ -1,4 +1,5 @@
 var Buffer = require('safe-buffer').Buffer
+var bech32 = require('bech32')
 var bs58check = require('bs58check')
 var bscript = require('./script')
 var networks = require('./networks')
@@ -16,6 +17,28 @@ function fromBase58Check (address) {
   return { hash: hash, version: version }
 }
 
+function fromBech32 (address, expectedPrefix) {
+  var result = bech32.decode(address)
+  var prefix = result.prefix
+  var words = result.words
+  if (expectedPrefix !== undefined) {
+    if (prefix !== expectedPrefix) throw new Error('Expected ' + expectedPrefix + ', got ' + prefix)
+  }
+
+  var version = words[0]
+  if (version > 16) throw new Error('Invalid version (' + version + ')')
+  var program = bech32.fromWords(words.slice(1))
+
+  if (version === 0) {
+    if (program.length !== 20 && program.length !== 32) throw new Error('Unknown program')
+  } else {
+    if (program.length < 2) throw new Error('Program too short')
+    if (program.length > 40) throw new Error('Program too long')
+  }
+
+  return { version, prefix, program: Buffer.from(program) }
+}
+
 function toBase58Check (hash, version) {
   typeforce(types.tuple(types.Hash160bit, types.UInt8), arguments)
 
@@ -24,6 +47,21 @@ function toBase58Check (hash, version) {
   hash.copy(payload, 1)
 
   return bs58check.encode(payload)
+}
+
+function toBech32 (prefix, version, program) {
+  if (version > 16) throw new Error('Invalid version (' + version + ')')
+  if (version === 0) {
+    if (program.length !== 20 && program.length !== 32) throw new Error('Unknown program')
+  } else {
+    if (program.length < 2) throw new Error('Program too short')
+    if (program.length > 40) throw new Error('Program too long')
+  }
+
+  var words = bech32.toWords(program)
+  words.unshift(version)
+
+  return bech32.encode(prefix, words)
 }
 
 function fromOutputScript (outputScript, network) {
@@ -47,7 +85,9 @@ function toOutputScript (address, network) {
 
 module.exports = {
   fromBase58Check: fromBase58Check,
+  fromBech32: fromBech32,
   fromOutputScript: fromOutputScript,
   toBase58Check: toBase58Check,
+  toBech32: toBech32,
   toOutputScript: toOutputScript
 }
