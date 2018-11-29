875e08a0334d31374061a7c257c81c04f67b904a
Daniel Cousens <dcousens@users.noreply.github.com>
2018-11-29 15:04:41 +1100

    add hardware wallet example

--- test/integration/transactions.js
+++ test/integration/transactions.js
@@ -3,6 +3,7 @@ const assert = require('assert')
 const bitcoin = require('../../')
 const regtestUtils = require('./_regtest')
 const regtest = regtestUtils.network
+const tinysecp = require('tiny-secp256k1')
 
 function rng () {
   return Buffer.from('YT8dAtK4d16A3P1z+TpwB2jJ4aFH3g9M1EioIBkLEV4=', 'base64')
@@ -20,7 +21,7 @@ describe('bitcoinjs-lib (transactions)', function () {
 
     txb.sign(0, alice)
 
-    // prepare for broadcast to the Bitcoin network, see "can broadcast a Transaction" below
+    // prepare for broadcast to the Bitcoin network, see "can broadcast via 3PBP"
     assert.strictEqual(txb.build().toHex(), '01000000019d344070eac3fe6e394a16d06d7704a7d5c0a10eb2a2c16bc98842b7cc20d561000000006b48304502210088828c0bdfcdca68d8ae0caeb6ec62cd3fd5f9b2191848edae33feb533df35d302202e0beadd35e17e7f83a733f5277028a9b453d525553e3f5d2d7a7aa8010a81d60121029f50f51d63b345039a290c94bffd3180c99ed659ff6ea6b1242bca47eb93b59fffffffff01e02e0000000000001976a91406afd46bcdfd22ef94ac122aa11f241244a37ecc88ac00000000')
   })
 
@@ -39,7 +40,7 @@ describe('bitcoinjs-lib (transactions)', function () {
     txb.sign(1, bob) // Bob signs his input, which was the second input (1th)
     txb.sign(0, alice) // Alice signs her input, which was the first input (0th)
 
-    // prepare for broadcast to the Bitcoin network, see "can broadcast a Transaction" below
+    // prepare for broadcast to the Bitcoin network, see "can broadcast via 3PBP" below
     assert.strictEqual(txb.build().toHex(), '01000000024c94e48a870b85f41228d33cf25213dfcc8dd796e7211ed6b1f9a014809dbbb5060000006a473044022041450c258ce7cac7da97316bf2ea1ce66d88967c4df94f3e91f4c2a30f5d08cb02203674d516e6bb2b0afd084c3551614bd9cec3c2945231245e891b145f2d6951f0012103e05ce435e462ec503143305feb6c00e06a3ad52fbf939e85c65f3a765bb7baacffffffff3077d9de049574c3af9bc9c09a7c9db80f2d94caaf63988c9166249b955e867d000000006b483045022100aeb5f1332c79c446d3f906e4499b2e678500580a3f90329edf1ba502eec9402e022072c8b863f8c8d6c26f4c691ac9a6610aa4200edc697306648ee844cfbc089d7a012103df7940ee7cddd2f97763f67e1fb13488da3fbdd7f9c68ec5ef0864074745a289ffffffff0220bf0200000000001976a9147dd65592d0ab2fe0d0257d571abf032cd9db93dc88ac10980200000000001976a914c42e7ef92fdb603af844d064faad95db9bcdfd3d88ac00000000')
   })
 
@@ -256,6 +257,27 @@ describe('bitcoinjs-lib (transactions)', function () {
         }, done)
       })
     })
+  })
+
+  // CAUTION: if you don't understand what this example is for, don't do it
+  it('can sign a Transaction with something other than ECPair', function () {
+    const txb = new bitcoin.TransactionBuilder()
+
+    txb.setVersion(1)
+    txb.addInput('61d520ccb74288c96bc1a2b20ea1c0d5a704776dd0164a396efec3ea7040349d', 0) // Alice's previous transaction output, has 15000 satoshis
+    txb.addOutput('1cMh228HTCiwS8ZsaakH8A8wze1JR5ZsP', 12000)
+    // (in)15000 - (out)12000 = (fee)3000, this is the miner fee
+
+    txb.sign(0, {
+      publicKey: Buffer.from('029f50f51d63b345039a290c94bffd3180c99ed659ff6ea6b1242bca47eb93b59f', 'hex'),
+      sign: function (hash) {
+        const privateKey = Buffer.from('8c112cf628362ecf4d482f68af2dbb50c8a2cb90d226215de925417aa9336a48', 'hex')
+        return tinysecp.sign(hash, privateKey)
+      }
+    })
+
+    // prepare for broadcast to the Bitcoin network, see "can broadcast via 3PBP"
+    assert.strictEqual(txb.build().toHex(), '01000000019d344070eac3fe6e394a16d06d7704a7d5c0a10eb2a2c16bc98842b7cc20d561000000006b48304502210088828c0bdfcdca68d8ae0caeb6ec62cd3fd5f9b2191848edae33feb533df35d302202e0beadd35e17e7f83a733f5277028a9b453d525553e3f5d2d7a7aa8010a81d60121029f50f51d63b345039a290c94bffd3180c99ed659ff6ea6b1242bca47eb93b59fffffffff01e02e0000000000001976a91406afd46bcdfd22ef94ac122aa11f241244a37ecc88ac00000000')
   })
 
   it('can verify Transaction signatures', function () {
