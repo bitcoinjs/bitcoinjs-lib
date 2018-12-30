e7ac2b9a4ec703aca840fa0517406a5bbf1d5c38
junderw <junderwood@bitcoinbank.co.jp>
2018-12-30 15:08:29 +0900

    pull in bip32 typescript

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,28 @@
-const bip32 = require('bip32')
-
-import { Block } from './block'
+import * as bip32 from 'bip32'
 import * as ECPair from './ecpair'
-import { Transaction } from './transaction'
-import { TransactionBuilder } from './transaction_builder'
 import * as address from './address'
 import * as crypto from './crypto'
 import * as networks from './networks'
 import * as payments from './payments'
 import * as script from './script'
-import { OPS as opcodes } from './script'
 
 export {
-  Block,
   ECPair,
-  Transaction,
-  TransactionBuilder,
   address,
   bip32,
   crypto,
   networks,
-  opcodes,
   payments,
   script,
 }
+
+export { Block } from './block'
+export { Transaction } from './transaction'
+export { TransactionBuilder } from './transaction_builder'
+export { OPS as opcodes } from './script'
 
 export { Payment, PaymentOpts } from './payments'
 export { Input as TxInput, Output as TxOutput } from './transaction'
 export { Network } from './networks'
 export { OpCode } from './script'
+export { BIP32Interface } from 'bip32'
