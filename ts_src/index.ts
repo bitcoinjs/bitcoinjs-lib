9ee7835ca6d46874e15e31094bfdb2861eed931a
Vis Virial <visvirial@gmail.com>
2019-04-28 13:08:09 +0900

    export classify.ts.

--- ts_src/index.ts
+++ ts_src/index.ts
@@ -1,12 +1,13 @@
 import * as bip32 from 'bip32';
 import * as address from './address';
+import * as classify from './classify';
 import * as crypto from './crypto';
 import * as ECPair from './ecpair';
 import * as networks from './networks';
 import * as payments from './payments';
 import * as script from './script';
 
-export { ECPair, address, bip32, crypto, networks, payments, script };
+export { ECPair, address, bip32, classify, crypto, networks, payments, script };
 
 export { Block } from './block';
 export { OPS as opcodes } from './script';
