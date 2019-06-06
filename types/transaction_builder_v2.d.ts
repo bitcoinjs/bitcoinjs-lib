ba40c1e02cdfba65ae8b460b9c12ea266ebe7300
Luke Childs <lukechilds123@gmail.com>
2019-06-06 17:10:57 +0700

    Don't extend TransactionBuilderV2 from TransactionBuilder

--- types/transaction_builder_v2.d.ts
+++ types/transaction_builder_v2.d.ts
@@ -1,8 +1,29 @@
+/// <reference types="node" />
+import { ECPairInterface } from './ecpair';
 import { Network } from './networks';
-import { TransactionBuilder } from './transaction_builder';
-export declare class TransactionBuilderV2 extends TransactionBuilder {
+import { Transaction } from './transaction';
+export declare class TransactionBuilderV2 {
     network: Network;
     maximumFeeRate: number;
-    static fromPsbtString(psbtString: string, network?: Network): TransactionBuilder;
+    static fromTransaction(transaction: Transaction, network?: Network): TransactionBuilderV2;
+    static fromPsbtString(psbtString: string, network?: Network): TransactionBuilderV2;
+    private __PREV_TX_SET;
+    private __INPUTS;
+    private __TX;
+    private __USE_LOW_R;
     constructor(network?: Network, maximumFeeRate?: number);
+    setLowR(setting?: boolean): boolean;
+    setLockTime(locktime: number): void;
+    setVersion(version: number): void;
+    addInput(txHash: Buffer | string | Transaction, vout: number, sequence?: number, prevOutScript?: Buffer): number;
+    addOutput(scriptPubKey: string | Buffer, value: number): number;
+    build(): Transaction;
+    buildIncomplete(): Transaction;
+    sign(vin: number, keyPair: ECPairInterface, redeemScript?: Buffer, hashType?: number, witnessValue?: number, witnessScript?: Buffer): void;
+    private __addInputUnsafe;
+    private __build;
+    private __canModifyInputs;
+    private __needsOutputs;
+    private __canModifyOutputs;
+    private __overMaximumFees;
 }
