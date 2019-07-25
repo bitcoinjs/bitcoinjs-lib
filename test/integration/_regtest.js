a3b6cdabb993ba5a1e20ed22f95bf22f057414b9
junderw <junderwood@bitcoinbank.co.jp>
2019-07-25 17:13:36 +0900

    Update regtest-client

--- test/integration/_regtest.js
+++ test/integration/_regtest.js
@@ -1,9 +1,8 @@
 const { RegtestUtils } = require('regtest-client')
 
-const bitcoin = require('../..')
 const APIPASS = process.env.APIPASS || 'satoshi'
 const APIURL = process.env.APIURL || 'https://regtest.bitbank.cc/1'
 
-const regtestUtils = new RegtestUtils(bitcoin, { APIPASS, APIURL })
+const regtestUtils = new RegtestUtils({ APIPASS, APIURL })
 
 module.exports = regtestUtils;
