7a3b21ea1da95b7266ad871ac203a22f08af9a8d
Nikos Fotiadis <nikosfotiadis0@gmail.com>
2021-01-13 12:21:15 +0200

    Add bitcoin diamond network

--- src/networks.js
+++ src/networks.js
@@ -33,3 +33,13 @@ exports.testnet = {
   scriptHash: 0xc4,
   wif: 0xef,
 };
+exports.bitcoindiamond = {
+  messagePrefix: '\x18BitcoinDiamond Signed Message:\n',
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x0488ade4
+  },
+  pubKeyHash: 0x00,
+  scriptHash: 0x05,
+  wif: 0x80
+};
