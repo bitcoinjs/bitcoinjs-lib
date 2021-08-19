0c11e0ef579b8f2806e174d1758157153f6606df
Firas Mefteh <ben.meft0@gmail.com>
2021-08-19 02:13:59 +0000

    Fix indent

--- test/integration/transactions.spec.ts
+++ test/integration/transactions.spec.ts
@@ -24,7 +24,7 @@ describe('bitcoinjs-lib (transactions with psbt)', () => {
 
       // non-segwit inputs now require passing the whole previous tx as Buffer
       nonWitnessUtxo: Buffer.from(
-        '0200000001f9f34e95b9d5c8abcd20fc5bd4a825d1517be62f0f775e5f36da944d9' +
+          '0200000001f9f34e95b9d5c8abcd20fc5bd4a825d1517be62f0f775e5f36da944d9' +
           '452e550000000006b483045022100c86e9a111afc90f64b4904bd609e9eaed80d48' +
           'ca17c162b1aca0a788ac3526f002207bb79b60d4fc6526329bf18a77135dc566020' +
           '9e761da46e1c2f1152ec013215801210211755115eabf846720f5cb18f248666fec' +
