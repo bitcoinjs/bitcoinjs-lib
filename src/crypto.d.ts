cc1b23e74ee52c362908b3e4ceda6a3ff95a6760
Vodopyanov Egor <egorvodopyanov@gmail.com>
2023-01-19 18:28:47 +0800

    fix: commit crypto.d.ts

--- src/crypto.d.ts
+++ src/crypto.d.ts
@@ -6,9 +6,10 @@ export declare function hash160(buffer: Buffer): Buffer;
 export declare function hash256(buffer: Buffer): Buffer;
 export declare const TAGS: readonly ["BIP0340/challenge", "BIP0340/aux", "BIP0340/nonce", "TapLeaf", "TapBranch", "TapSighash", "TapTweak", "KeyAgg list", "KeyAgg coefficient"];
 export type TaggedHashPrefix = typeof TAGS[number];
-export type TaggedHashPrefixes = {
+type TaggedHashPrefixes = {
     [key in TaggedHashPrefix]: Buffer;
 };
 /** An object mapping tags to their tagged hash prefix of [SHA256(tag) | SHA256(tag)] */
 export declare const TAGGED_HASH_PREFIXES: TaggedHashPrefixes;
 export declare function taggedHash(prefix: TaggedHashPrefix, data: Buffer): Buffer;
+export {};
