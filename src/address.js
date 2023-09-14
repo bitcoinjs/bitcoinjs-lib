d60ba49a3c92c8ce2a2d350f17cbe66417f85e8c
Waqas Ahmed <wakqasahmed@gmail.com>
2023-09-14 22:12:45 +0500

    Fixes issue#1974
    Wrong method call fixed for the issue https://github.com/bitcoinjs/bitcoinjs-lib/issues/1974

--- src/address.js
+++ src/address.js
@@ -46,7 +46,7 @@ function fromBase58Check(address) {
   // TODO: 4.0.0, move to "toOutputScript"
   if (payload.length < 21) throw new TypeError(address + ' is too short');
   if (payload.length > 21) throw new TypeError(address + ' is too long');
-  const version = payload.readUint8(0);
+  const version = payload.readUInt8(0);
   const hash = payload.slice(1);
   return { version, hash };
 }
