8bf16e95dc04c1eb3d877b4b4bd25985f5efdc25
PeterParker <yihang_i@163.com>
2024-12-25 16:14:42 +0800

    test: add decodes a series of numbers from '82 to 96' correctly

--- test/script.spec.ts
+++ test/script.spec.ts
@@ -50,6 +50,18 @@ describe('script', () => {
         Uint8Array.from([OPS.OP_RETURN, OPS.OP_FALSE]),
       );
     });
+
+    it("decodes a series of numbers from '82 to 96' correctly", () => {
+      const asm = Array.from({ length: 15 }, (_, i) =>
+        (i + 130).toString(16).padStart(2, '0'),
+      ).join(' ');
+      const expected = Array.from({ length: 15 }, (_, i) => [
+        1,
+        i + 130,
+      ]).flat();
+      const result = bscript.fromASM(asm);
+      assert.deepStrictEqual(result, Uint8Array.from(expected));
+    });
   });
 
   describe('toASM', () => {
